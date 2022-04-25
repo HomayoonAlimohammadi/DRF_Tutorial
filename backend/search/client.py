1d098d81c5b3ae96d1fe96eecc2e24bee7b43f4d
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-04-25 14:44:05 +0430

    Add Search

---
+++ backend/search/client.py
@@ -0,0 +1,17 @@
+from algoliasearch_django import algolia_engine
+
+
+def get_client():
+    return algolia_engine.client
+
+
+def get_index(index_name='cfe_Product'):
+    client = get_client()
+    index = client.init_index(index_name)
+    return index
+
+
+def perform_search(query, **kwargs):
+    index = get_index()
+    results = index.search(query)
+    return results
