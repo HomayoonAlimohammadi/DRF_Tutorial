c3687ae37ae650b90a8ebaa1f750fc00da0fc651
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-04-25 19:06:15 +0430

    Add JS Client

---
+++ js_client/client.js
@@ -0,0 +1,35 @@
+// to run this html head to this directory and:
+// python -m http.server 8111 
+
+
+const loginForm = document.getElementById('login-form')
+const baseEndpoint = 'http://localhost:8000/api'
+
+if (loginForm) {
+    loginForm.addEventListener('submit', handleLogin)
+}
+
+function handleLogin(event) {
+
+    event.preventDefault()
+    const loginEndpoint = `${baseEndpoint}/auth/`
+    let loginFormData = new FormData(loginForm)
+    let loginObjectData = Object.fromEntries(loginFormData)
+    let bodyStr = JSON.stringify(loginObjectData)
+    const options = {
+        method: 'POST',
+        headers: {
+            'ContentType': 'application/json'
+        },
+        body: bodyStr
+    }
+    fetch(loginEndpoint, options)
+    .then(response => {
+        console.log(response)
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+
+}
